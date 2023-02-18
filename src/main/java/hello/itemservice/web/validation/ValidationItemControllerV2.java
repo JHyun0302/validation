cb0f22a59d54c4c7b5ff3298a49588a49bc5fbd4
jaehyun <wogustkfkd11@gmail.com>
2023-02-18 21:15:13 +0900

    검증기 도입

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -29,7 +29,6 @@ public class ValidationItemControllerV2 {
     @InitBinder
     public void init(WebDataBinder dataBinder) {
         dataBinder.addValidators(itemValidator);
-        //@InitBinder가 호출될 때마다 항상 검증기(itemValidator)를 자동으로 적용할 수 있다.
     }
 
     @GetMapping
@@ -51,6 +50,11 @@ public class ValidationItemControllerV2 {
         model.addAttribute("item", new Item());
         return "validation/v2/addForm";
     }
+
+    /**
+     * 오류 메시지 출력(BindingResult)
+     * 문제는 사용자의 타입 오류같은 경우 담을 공간이 따로 없음!
+     */
 
     //    @PostMapping("/add")
     public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
@@ -89,6 +93,12 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
+    /**
+     * 타입 오류 발생시 new FieldError()에 잘못된 에러 값 저장 후 BindingResult에 담아서 컨트롤러 호출!
+     * 그래서 사용자의 타입 오류 같은 바인딩 실패시 오류 메시지 정상 출력 가능!!
+     * <p>
+     * rejectedValue: 오류 발생시 사용자가 입력한 값을 저장!
+     */
     //    @PostMapping("/add")
     public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         //item 모델에 담다가 오류가 생기면 bindingResult에 담김! 검증 오류 결과를 보관 (errors == bindingResult)
@@ -96,7 +106,7 @@ public class ValidationItemControllerV2 {
         //검증 로직
         if (!StringUtils.hasText(item.getItemName())) {
             bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품 이름은 필수입니다."));
-        } //objectName: @ModelAttribute, field: 필드 이름, rejectedValue: 사용자가 넣은 오류 내용, bindingFailure: 바인딩(item객체에 값이 들어갔는지) 성공여부, cdes: 오류 메시지 한 곳에서 보관, arguments: 파라미터 값, defaultMessage: 메시지
+        } //objectName: @ModelAttribute, field: 필드 이름, rejectedValue: 사용자가 넣은 오류 내용, bindingFailure: 바인딩(item객체에 값이 들어갔는지) 성공여부, codes: 오류 메시지 한 곳에서 보관, arguments: 파라미터 값, defaultMessage: 메시지
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 100000) {
             bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null, "가격은 1,000 ~ 1,000,000 까지 허용합니다."));
         }
@@ -125,12 +135,15 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
+    /**
+     * 오류 메시지 "error.properties" 사용 new FieldError(codes, arguments) 삽입
+     */
+
     //    @PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
-        //item 모델에 담다가 오류가 생기면 bindingResult에 담김! 검증 오류 결과를 보관 (errors == bindingResult)
 
-        log.info("objectName={}", bindingResult.getObjectName()); //bindingResult는 이미 자신이 바인딩 할 객체를 알고 있음!
-        log.info("target={}", bindingResult.getTarget()); //target도 이미 item객체 value를 가지고 있음!
+        log.info("objectName={}", bindingResult.getObjectName());
+        log.info("target={}", bindingResult.getTarget());
         if (!StringUtils.hasText(item.getItemName())) {
             bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, new String[]{"required.item.itemName"}, null, null));
         } //objectName: @ModelAttribute, field: 필드 이름, rejectedValue: 사용자가 넣은 오류 내용, bindingFailure: 바인딩(item객체에 값이 들어갔는지) 성공여부, cdes: 오류 메시지 한 곳에서 보관, arguments: 파라미터 값, defaultMessage: 메시지
@@ -162,30 +175,36 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
+    /**
+     * bindingResult는 이미 자신이 바인딩 할 객체(target)을 알고 있다.
+     * 따라서 FieldError(), ObjectError() 필요없음! => 대신 rejectValue(), reject() 사용!
+     * <p>
+     * rejectValue() 사용시 errorCode를 range만 작성해도 됨! (V3: 모두 작성) -> MessageCodesResolver 작동(errors.properties)
+     */
+
     //    @PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
-        //item 모델에 담다가 오류가 생기면 bindingResult에 담김! 검증 오류 결과를 보관 (errors == bindingResult)
 
         log.info("objectName={}", bindingResult.getObjectName()); //bindingResult는 이미 자신이 바인딩 할 객체를 알고 있음!
         log.info("target={}", bindingResult.getTarget()); //target도 이미 item객체 value를 가지고 있음!
 
         ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "itemName", "required"); //아래 있는 if문과 같은 뜻
         /*if (!StringUtils.hasText(item.getItemName())) {
-            bindingResult.rejectValue("itemName", "required");
+            bindingResult.rejectValue("itemName", "required"); //("field 명", "errorcode")
         } */
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 100000) {
             bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
         }
         if (item.getQuantity() == null || item.getQuantity() >= 9999) {
             bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
-        } //rejectValue는 fielderror일 때 사용
+        } //rejectValue()는 fielderror()일 때 사용
 
         //특정 필드가 아닌 복합 룰 검증
         if (item.getPrice() != null && item.getQuantity() != null) {
             int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000) {
                 bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
-            }//reject는 obejcterror일 때 사용
+            }//reject()는 obejcterror()일 때 사용
         }
 
         //검증에 실패하면 다시 입력 폼으로
@@ -201,9 +220,14 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
+    /**
+     * Validator 분리1
+     */
+
     //    @PostMapping("/add")
     public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
+        //검증기
         itemValidator.validate(item, bindingResult);
 
         //검증에 실패하면 다시 입력 폼으로
@@ -218,6 +242,13 @@ public class ValidationItemControllerV2 {
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v2/items/{itemId}";
     }
+
+    /**
+     * WebDataBinder: 해당 컨트롤러에서는 검증기를 자동으로 적용!
+     *
+     * @InitBinder -> 해당 컨트롤러에만 영향준다. 글로벌 설정 X
+     * @Validated: 자동으로 검증기 적용
+     */
 
     @PostMapping("/add")
     public String addItemV6(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
